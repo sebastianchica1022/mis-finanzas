3c3a3898c6923fc205c6d59bb4ca05661207a289
sebastianchica1022 <sebastianchica1022@gmail.com>
2026-08-15 18:34:41 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mis-finanzas-v1';
+const CACHE_NAME = 'mis-finanzas-v2';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
